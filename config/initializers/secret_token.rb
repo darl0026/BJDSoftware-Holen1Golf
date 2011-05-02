ea49945b183709ff1fdf49cafe1c5b211859c5b4
Darling, Brandon <trigmafia3250@hotmail.com>
2011-05-02 14:59:49 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holen1Golf::Application.config.secret_token = '40729eeed5670094941821d12beb1f3e9fe20ac9af7c0f1e9ef7c21537182c14cd60d344548363cd9390da0104ce3be99d8d20e45b91524e9b248e0e8255f843'
